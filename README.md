1934cb79c8ed26870ff1bce7954a3ba6019aacfd
Eric Tipton <eric.tipton@grayscaleapp.com>
2021-07-07 13:45:36 -0400

    Copy project files from replit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# its-marta
+# MARTA-arrivals
+
+## Overview
+
+For those who are not familiar, MARTA stands for Metropolitan Atlanta Rapid Transit Authority.
+
+In this project, you'll be filtering data from the [MARTA Rail Realtime RESTful API](https://www.itsmarta.com/app-developer-resources.aspx).
+
+The intent is to poll the API periodically and write the filtered data to a file (`out.json`). See the "Goals" section below for details.
+
+The provided code is in [go](https://golang.org/) but feel free to start a new project from scratch if you'd prefer to use another language.
+
+## Setup
+
+You should be able to run the project as-is without any issues connecting to the API.
+
+If, however, you encounter a permissions issue, feel free to [register for your own key](https://www.itsmarta.com/developer-reg-rtt.aspx) and [update the API_KEY Secret value](https://docs.replit.com/repls/secrets-environment-variables#secrets-environment-variables).
+
+## Goals
+- Imagine you've taken over this project while the original author is on vacation. Update (or rewrite in another language) as if for a real production service. (For example, assume any [stdout output will be archived](https://12factor.net/logs).)
+  - Consider resiliency, idempotency, security, and error-handling.
+  - Audit for pre-existing bugs / easy-to-fix inefficiencies!
+- Filter the original API response to only the "TRAIN_ID", "STATION", and "WAITING_SECONDS" key-value pairs. (Case-sensitivity of the resulting key/val strings is NOT important, however.)
+- Implement the `filterStops` method to further filter to only items where `WAITING_SECONDS` is less than `120`.
+  - This is intended to be where you'll do the most actual [go](https://golang.org/) coding. All other updates (that we're looking for, at least) should require minimal _new_ code!
+- Importantly, to minimize disk space, this service should NOT write to `out.json` any MORE frequently than once every `10` seconds. Writing LESS-frequently, on the other hand, is acceptable, if needed, to improve resiliency.
+- Handling interrupts / signals is outside the scope of this exercise, BUT -- you should assume that if the process dies, it will immediately be restarted!
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "net/http" // TODO: use "github.com/hashicorp/[redacted]" instead?
+  "io/ioutil"
+  "log"
+  "time"
+  "encoding/json"
+  "github.com/replit/database-go"
+)
+
+var out *os.File
+
+var baseUrl = "http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey=%s"
+var reqUrl = fmt.Sprintf(baseUrl, os.Getenv("API_KEY"))
+var httpClient = &http.Client{}
+// var betterHttpClient = [redacted]
+
+var timeStrLayout = "2006-01-02 15:04:05.999999999 -0700 MST"
+var lastWriteKey = "lastWrite"
+
+type Stop struct {
+  TrainId string
+  Waiting_Seconds string
+}
+
+func main() {
+  var err error
+  out, err = os.OpenFile("out.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+  checkError(err)
+  defer out.Close()
+
+  // betterHttpClient.Logger = [redacted] #security
+
+  lastWriteFinished := make(chan bool) // this will serve as our mutex
+  for {
+    lastWrite, err := getLastWriteTime()
+    if (err != nil) {
+      lastWrite = time.Now()
+      setLastWriteTime(lastWrite)
+    }
+
+    if (time.Now().Sub(lastWrite) >= (0 * time.Second)) { // hmm, this seems wrong
+      go writeFilteredStops(lastWriteFinished)
+      <-lastWriteFinished // "release" mutex
+      time.Sleep(10 * time.Second)
+    }
+  }
+}
+
+func getLastWriteTime() (time.Time, error) {
+  lastWriteStr, _ := database.Get(lastWriteKey)
+  return time.Parse(timeStrLayout, lastWriteStr)
+}
+
+func setLastWriteTime(t time.Time) {
+  database.Set(lastWriteKey, t.Format(timeStrLayout))
+}
+
+func writeFilteredStops(finished chan bool) {
+  stops := filterStops(fetchStops())
+
+  marshalled, err := json.Marshal(stops)
+  checkError(err)
+
+  out.Write(append(marshalled, '\n'))
+  out.Sync()
+  setLastWriteTime(time.Now())
+
+  finished <- true
+}
+
+func fetchStops() []Stop {
+  resp, err := httpClient.Get(reqUrl)
+  checkError(err)
+
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+  checkError(err)
+
+  var stops []Stop
+  err = json.Unmarshal(body, &stops)
+  checkError(err)
+  return stops
+}
+
+func filterStops(stops []Stop) []Stop {
+  return stops // TODO: filter to only stops w/ waiting_seconds < 120
+}
+
+func checkError(err error) {
+  if (err != nil) {
+    log.Panic(err)
+  }
+}
